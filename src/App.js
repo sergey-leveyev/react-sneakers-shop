7b0d8558a30790b82abe20e88239986832d604e3
unknown <sargey7@gmail.com>
2021-08-13 20:20:12 +0300

    130821

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 function App() {
   return (
     <div className="wrapper clear">
-      <header className="d-flex justify-between align-center">
+      {/* HEADER START */}
+      <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" />
           <div>
-            <h3>React Sneakers</h3>
-            <p>best shoe shope</p>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">best shoe shope</p>
           </div>
         </div>
         <ul className="d-flex">
@@ -20,9 +21,73 @@ function App() {
           </li>
         </ul>
       </header>
-      <div className="content">
-        <h1>all shoe</h1>
+      {/* HEADER END */}
+
+      {/* CONTENT START */}
+      <div className="content p-40">
+        <h1 className="mb-40">all shoe</h1>
+
+<div className="d-flex">
+          {/* CARD START */}
+          <div className="card">
+          <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
+          <h5>It is a long established fact that a reader</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>price: </span>
+              <b>556 NIS.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/2.jpg" alt="" />
+          <h5>It is a long established fact that a reader</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>price: </span>
+              <b>556 NIS.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/3.jpg" alt="" />
+          <h5>It is a long established fact that a reader</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>price: </span>
+              <b>556 NIS.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/4.jpg" alt="" />
+          <h5>It is a long established fact that a reader</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>price: </span>
+              <b>556 NIS.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
+        {/* CARD END */}
+</div>
       </div>
+      {/* CONTENT END */}
     </div>
   );
 }
