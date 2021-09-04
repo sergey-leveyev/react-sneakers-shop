45a98170b452a94078d3d13837bec51719ad7022
unknown <sargey7@gmail.com>
2021-09-04 11:00:16 +0300

    040921

--- src/App.js
+++ src/App.js
@@ -4,43 +4,60 @@ function App() {
       <div className="overlay">
         <div className="drawer">
           <h2 className="mb-30">Cart</h2>
+          <div className="items">
+            <div className="cartItem d-flex align-center  mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
 
-          <div className="cartItem d-flex align-center  mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              className="cartItemImg"
-            ></div>
-
-            <div className="mr-20">
-              <p className="mb-5 flex">
-                Lorem Ipsum is simply dummy text of printing
-              </p>
-              <b>556 NIS</b>
+              <div className="mr-20">
+                <p className="mb-5 flex">
+                  Lorem Ipsum is simply dummy text of printing
+                </p>
+                <b>556 NIS</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="btn-remove"
+              />
             </div>
-            <img
-              className="removeBtn"
-              src="/img/btn-remove.svg"
-              alt="btn-remove"
-            />
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20">
+                <p className="mb-5 flex">
+                  Lorem Ipsum is simply dummy text of printing
+                </p>
+                <b>556 NIS</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="btn-remove"
+              />
+            </div>
           </div>
 
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              className="cartItemImg"
-            ></div>
-
-            <div className="mr-20">
-              <p className="mb-5 flex">
-                Lorem Ipsum is simply dummy text of printing
-              </p>
-              <b>556 NIS</b>
-            </div>
-            <img
-              className="removeBtn"
-              src="/img/btn-remove.svg"
-              alt="btn-remove"
-            />
+          <div className="cardTotalBlock">
+            <ul>
+              <li>
+                <span>Subtotal:</span>
+                <div></div>
+                <b>1000 NIS</b>
+              </li>
+              <li>
+                <span>shipping</span>
+                <div></div>
+                <b>30 NIS</b>
+              </li>
+            </ul>
+            <button>Buy</button>
           </div>
         </div>
       </div>
